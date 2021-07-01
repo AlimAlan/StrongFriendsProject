ef9933c90dd7c0200dee1b70c320ee2d9451a823
Aizat Kalybekova <aizat.kalybekova@yahoo.com>
2021-07-01 13:14:24 -0500

    added iq

---
+++ src/test/java/aizat/RemoveDuplicate.java
@@ -0,0 +1,25 @@
+package aizat;
+
+public class RemoveDuplicate {
+    /**
+     * remove duplicates from String
+     */
+    public static void main(String[] args) {
+        String word = "aplle";
+        String uniq = "";
+
+        for(char each : word.toCharArray()) {
+            if(uniq.indexOf(each) != -1){
+                continue;
+            }else{
+                uniq+= each; // (meaning) uniq = uniq +each
+            }
+
+
+        }
+        System.out.println(uniq);
+
+    }
+}
+
+
