571c872eafaa54e64165077c951ca0a3b7531cb0
GeryaM <geryam112@gmail.com>
2021-06-30 22:51:12 -0400

    new sout was added

--- src/test/java/Masha/MashasClass.java
+++ src/test/java/Masha/MashasClass.java
@@ -1,7 +1,10 @@
 package Masha;
 
+import javax.sound.midi.Soundbank;
+
 public class MashasClass {
     public static void main(String[] args) {
         System.out.println("Hello Strong Friends");
+        System.out.println("Updates is coming");
     }
 }
